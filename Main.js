a0df44dfa1c629f0adc99bf1b39112dc76db4d10
Alexis28J <jordy2806@hotmail.com>
2025-11-24 13:26:51 +0100

    Fine lezione (24.11.2025)

--- Main.js
+++ Main.js
@@ -86,6 +86,184 @@ console.log(barolo.price); //devo vedere il prezzo calcolato con la formula del 
 
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//DOM: DOCUMENT OBJECT MODEL
+// Il Document Object Model (DOM) serve a rappresentare una pagina web (HTML/XML) come un albero di oggetti che JavaScript può manipolare. 
+// Questo permette agli sviluppatori di modificare dinamicamente il contenuto, la struttura e lo stile di una pagina in tempo reale, rendendola interattiva senza dover ricaricare la pagina. 
+// È come una mappa del documento che JavaScript può usare per interagire con i suoi elementi. 
+
+
+console.log(document);    
+
+const h1Title = document.getElementById('main-title'); //prendo l'elemento che voglio cambiare
+console.log(h1Title);
+
+h1Title.innerText = "Enoteca Bartolletti S.P.A.";  //Ho cambiato il titolo del html
+
+//document.getElementById() serve per selezionare un singolo elemento HTML in una pagina web tramite il suo attributo id.
+
+//La proprietà .innerText di JavaScript serve per ottenere o impostare il contenuto testuale visibile all'interno di un elemento HTML,
+//ignorando il markup HTML e rispettando lo stile CSS (ad esempio, gli elementi nascosti sono esclusi).
+//È utile per leggere o scrivere testo puro, comportandosi come se si copiasse e incollato il contenuto visibile. 
+
+
+//creo un nuovo oggetto PremiumWine
+//const barbera = new PremiumWine("Barbera d'Asti", "Consorzio Barbera", 2020, 75, 12.5, "Barbera", "Piemonte", "D.O.C.", 18);  //Devo rispettare
+const barbera = new PremiumWine ("Barbera d'Asti", "Consorzio Barbera", 2020, 75, 12.5, 5, 18, "Barbera", "Piemonte", "D.O.C.");
+
+const nameSpan = document.getElementById('card-name');
+nameSpan.innerText = barbera.name;
+
+const producerSpan = document.getElementById('card-producer');
+producerSpan.innerText = barbera.producer;
+
+// const yobSpan = document.getElementById('card-yob');
+// yobSpan.innerText = barbera.yob;
+
+//const quantitySpan = document.getElementById('card-quantity');
+// quantitySpan.innerText = barbera.quantity;
+
+// const alcoholSpan = document.getElementById('card-alcohol');
+// alcoholSpan.innerText = barbera.alcohol;
+
+// const bottleNumberSpan = document.getElementById('card-bottleNumber');
+// bottleNumberSpan.innerText = barbera.bottleNumber;
+
+// const priceSpan = document.getElementById('card-price');
+// priceSpan.innerText = barbera.price;
+
+// const viteSpan = document.getElementById('card-vite');
+// viteSpan.innerText = barbera.vite;
+
+// const locationSpan = document.getElementById('card-location');
+// locationSpan.innerText = barbera.location;
+
+// const certificationSpan = document.getElementById('card-certification');
+// certificationSpan.innerText = barbera.certification;
+
+document.getElementById('card-yob').innerText = barbera.yob;  // sono uguale a quello di prima, solo che più corti
+document.getElementById('card-quantity').innerText = barbera.quantity;
+document.getElementById('card-alcohol').innerText = barbera.alcohol;
+document.getElementById('card-bottleNumber').innerText = barbera.bottleNumber; 
+document.getElementById('card-price').innerText = barbera.price;
+document.getElementById('card-vite').innerText = barbera.vite; 
+document.getElementById('card-location').innerText = barbera.location;; 
+document.getElementById('card-certification').innerText = barbera.certification;
+document.getElementById('card-yob').innerText = barbera.yob;
+
+
+//creo un'altro PremiumWine
+const champagne = new PremiumWine("Champagne Brut", "Charles Vercy", 2020, 75, 12, "undefined", 19.20, "Chardonnay", "Francia", "D.O.C.");  //ho messo "undefined" in bottleNumber perchè non si sposti il valore a un'altra proprietà 
+
+
+//voglio creare una nuova scheda
+const newCard = document.createElement('div');  //creo un elemento div  //ho messo div perché l'elemento che ha la tag 'card'
+// Il metodo createElement() in JavaScript crea un nuovo elemento HTML e lo aggiunge alla pagina web.
+newCard.className = 'card';  
+//La proprietà .className in JavaScript serve per leggere e manipolare l'attributo class di un elemento HTML. 
+
+const main = document.getElementById('main-content');
+main.appendChild(newCard);
+//Il metodo .appendChild() in JavaScript serve a inserire un nuovo nodo (un elemento HTML) alla fine di un altro elemento esistente (come ultimo figlio di un genitore specificato).
+//Dove main è l’elemento genitore e newCard è il nodo da inserire.
+
+const cardImage = document.createElement('img');  //creo un elemento 'img'
+cardImage.src = './Assets/Wine.svg';
+cardImage.width = '100';
+
+newCard.appendChild(cardImage);   //'cardImage' è un figlio di 'card' 
+
+
+//creo un nuovo elemento nel div 
+const nameContainer = document.createElement('div');
+
+//e questo 'div' devo metterlo alla 'newCard'
+newCard.appendChild(nameContainer);
+
+const nameKey = document.createElement('strong');
+nameKey.innerText = 'Nome: ';
+nameContainer.appendChild(nameKey);
+
+const nameValue = document.createElement('span');
+nameValue.innerText = champagne.name;
+nameContainer.appendChild(nameValue);
+
+
+//ora faccio lo stesso processo per il producer
+
+const producerContainer = document.createElement('div');
+newCard.appendChild(producerContainer)
+
+const producerKey = document.createElement('strong');
+producerKey.innerText = 'Produttore: ';
+producerContainer.appendChild(producerKey);
+
+const producerValue = document.createElement('span');
+producerValue.innerText = champagne.producer;
+producerContainer.appendChild(producerValue);
+
+//yob
+
+const yobContainer = document.createElement('div');
+newCard.appendChild(yobContainer)
+
+const yobKey = document.createElement('strong');
+yobKey.innerText = 'Annata: ';
+yobContainer.appendChild(yobKey);
+
+const yobValue = document.createElement('span');
+yobValue.innerText = champagne.yob;
+yobContainer.appendChild(yobValue);
+
+
+//quantity
+
+const quantityContainer = document.createElement('div');
+newCard.appendChild(quantityContainer)
+
+const quantityKey = document.createElement('strong');
+quantityKey.innerText = 'CL: ';
+quantityContainer.appendChild(quantityKey);
+
+const quantityValue = document.createElement('span');
+quantityValue.innerText = champagne.quantity;
+quantityContainer.appendChild(quantityValue);
+
+//alcohol
+
+const alcoholContainer = document.createElement('div');
+newCard.appendChild(alcoholContainer)
+
+const alcoholKey = document.createElement('strong');
+alcoholKey.innerText = 'Gradi: ';
+alcoholContainer.appendChild(alcoholKey);
+
+const alcoholValue = document.createElement('span');
+alcoholValue.innerText = champagne.alcohol;
+alcoholContainer.appendChild(alcoholValue);
+
+
+//price
+
+const priceContainer = document.createElement('div');
+newCard.appendChild(priceContainer)
+
+const priceKey = document.createElement('strong');
+priceKey.innerText = 'Prezzo: ';
+priceContainer.appendChild(priceKey);
+
+const priceValue = document.createElement('span');
+priceValue.innerText = champagne.price;
+priceContainer.appendChild(priceValue);
+
+
+
+
+
+/// COMPITO FARE LA BIRRA SEGUENDO LI STESSI PASSI DI CHAMPAGNE
+
+
 
 
 
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Esercizi di Oriented Object Programming
 
 Sintassi: `${ }`  // interpolazione di stringhe 
 Alt + 96: `
+PAGINA PER SCEGLIERE DEI BORDI DI VARI TIPI PER IL CSS: https://box-shadow.dev/
+DOM: Document Object Model
